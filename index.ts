e7de2db7d458e78bd02e29c07cb1491c137e67c4
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-10-04 14:59:36 +0200

    ✨ index > add simple routage

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ Bun.serve({
   hostname: "localhost",
   port: 3000,
   fetch(req: Request): Response {
-    return new Response("Hello World!")
+    const url = new URL(req.url)
+
+    if (url.pathname === "/ping") {
+      return new Response("pong")
+    }
+
+    return new Response(`you requested url ${req.url} with pathname ${url.pathname}`)
   },
 })
